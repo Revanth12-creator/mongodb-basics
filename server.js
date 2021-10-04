fd90e76f585fa49c40deb81df7289040a977c9d0
Revanth12-creator <revanth.sake6@gmail.com>
2021-10-04 18:59:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+let mongodb=require("mongodb");//this the mongodb driver which is used to connect mongodb server
+let url="mongodb://localhost:27017";//mongodb url
+
+mongodb.connect(url,{ useUnifiedTopology: true },(err,databse)=>{
+            if(err) throw err;
+            console.log("connected to databse ",url);
+            // result.close();
+
+            //now create list og documets to store in database
+            let employeeDetails=[
+                {
+                    name:"Revanth",
+                    age:22,
+                    company:"MNC",
+                    location:"banglure",
+                },
+                {
+                    name:"Kuamr",
+                    age:21,
+                    company:"MNC",
+                    location:"banglure",
+                },
+                {
+                    name:"sathis",
+                    age:32,
+                    company:"MNC",
+                    location:"banglure",
+                },
+                {
+                    name:"garu",
+                    age:24,
+                    company:"MNC",
+                    location:"banglure",
+                },
+
+                {
+                    name:"shambha",
+                    age:30,
+                    company:"MNC",
+                    location:"banglure",
+                },
+                {
+                    name:"hari",
+                    age:30,
+                    company:"MNC",
+                    location:"banglure",
+                },
+            ];
+
+            //now create DATABASE name
+            let dbname=databse.db("TCS");
+
+            //create collection name    //here inserting using insertmany
+            dbname.collection("Users").insertMany(employeeDetails,(err) => {
+                if(err) throw err;
+                console.log("succesfully inserted into database")
+            })
+
+        }
+)
